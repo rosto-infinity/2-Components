e1789ec686207ca8ebb9b50d408108d17b7a1994
ROSTAND LELE <rostoinfocus@gmail.com>
2024-12-24 06:52:15 +0100

    Initial commit

---
+++ src/Checkbox.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <input type="checkbox" v-model="checked" />
+    <label> Lorem, ipsum dolor.</label>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const checked = ref(false);
+</script>
